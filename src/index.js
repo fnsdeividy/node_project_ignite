fc41030c60491fef97b02a189cf271dd614eb0e2
deividy <deividyferreira.n@gmail.com>
2021-11-06 01:54:07 -0300

    Middlewares

--- src/index.js
+++ src/index.js
@@ -6,18 +6,30 @@ const PORT = 4000;
 
 app.use(express.json());
 
-const costumers = [];
+let customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers
+    const customer = customers.find(customer => (customer.cpf === cpf))
+    if(!customer) {
+        res.status(404).json({ error:'Costumer not found' })
+    }
+    req.customer = customer
+    return next()
+}
+
+//Routes
 app.post("/account", (req, res) => {
     const id = uuid();
     const { cpf, name } = req.body;
-    const costumerAlreadyExists = costumers.some(costumer => costumer.cpf === cpf)
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
 
-    if(costumerAlreadyExists) {
+    if(customerAlreadyExists) {
         return res.status(400).json({ error: 'Costumer already exists!' })
     }
 
-    costumers.push({
+    customers.push({
         cpf,
         name,
         id,
@@ -26,13 +38,10 @@ app.post("/account", (req, res) => {
     res.status(201).send();
 });
 
-app.get('/statement/', (req, res) => {
-    const { cpf } = req.headers
-    const costumer = costumers.find(costumer => (costumer.cpf === cpf))
-    if(!costumer) {
-        res.status(404).json({ error:'Costumer not found' })
-    }
-    res.status(200).send(costumer.statement)
+app.get('/statement/',verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req
+    
+    res.status(200).send(customer.statement)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
